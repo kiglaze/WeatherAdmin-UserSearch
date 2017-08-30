77e325159a4a1de7e75a96a4c72ccb07aaf5bb5a
Kristin Glaze <ikglaze33@gmail.com>
2017-08-30 19:45:40 -0400

    Accounted for argument that says user or admin. Also added start of print all user searches function in weather admin.

--- cli.js
+++ cli.js
@@ -1,5 +1,9 @@
+const ADMIN = "admin";
+const USER = "user";
 var args = process.argv;
-var name = args[2];
-var location = args[3];
+var isAdmin = (args[2] === ADMIN);
+var name = args[3];
+var location = args[4];
 var WeatherAdmin = require("./WeatherAdmin.js");
 var fs = require("fs");
+
